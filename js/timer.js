50a5c78f99c4c1a9d99a1cad6784bd0cef7d335f
Geol <big9401@gmail.com>
2022-03-04 13:50:22 +0900

    포지션 제거 플렉스 위치 변경

--- js/timer.js
+++ js/timer.js
@@ -1,3 +1,11 @@
+const hour = document.getElementById("hour");
+const minute = document.getElementById("minute");
+const second = document.getElementById("second");
+
+const hour_range = document.getElementById("range_hour");
+const min_range = document.getElementById("range_min");
+const sec_range = document.getElementById("range_sec");
+
 function startTimer() {
 	console.log(1);
 }
@@ -11,14 +19,6 @@ function stopTimer() {
 }
 
 function initTimer() {
-	const hour_range = document.getElementById("range_hour");
-	const min_range = document.getElementById("range_min");
-	const sec_range = document.getElementById("range_sec");
-
-	const hour = document.getElementById("hour");
-	const minute = document.getElementById("minute");
-	const second = document.getElementById("second");
-
 	hour.innerHTML = hour_range.value;
 	minute.innerHTML = min_range.value;
 	second.innerHTML = sec_range.value;
@@ -26,4 +26,4 @@ function initTimer() {
 
 initTimer();
 
-() => {};
+hour_range.addEventListener("", updateHour);
